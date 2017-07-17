85e561bd3e7457b89e4ab19bd8c94592b075618f
unknown <shivashirbhate@gmail.com>
2017-07-18 00:56:03 +0530

    added gitignore with some dbmodule changes

--- dbmodule.php
+++ dbmodule.php
@@ -5,7 +5,7 @@ class dbmodule{
 	var $username;
 	var $password;
 	var $database;
-
+	var $conn;
 	function __construct(){
 
 		$this->hostname = HOSTNAME;
@@ -15,22 +15,74 @@ class dbmodule{
 	}
 
 	function insert(){
+		// for insert data
+		$sql = "SELECT id, name1, link, description from table1";
+    // use exec() because no results are returned
+		$result = $this->conn->exec($sql);
+		var_dump($result);
+		$this->disconnect();
 
 	}
 
 	function getAllLinks(){
-		echo "links";
+		$this->connect();
+		$sql = "SELECT id, name1, link, description from table1";
+		$result = $this->conn->query($sql);
+		$final = json_encode($result->fetchAll(PDO::FETCH_ASSOC));
+		// if ($result->num_rows > 0) {
+			// echo $final;
+		// }
+		// echo gettype($final);
+		$ab = json_decode($final);
+		
+
+
+		$newArr = array();
+		foreach($ab as $key => $value) {
+			if($key == "name1"){
+				echo $key;
+				die();
+				$newArr[ $final[ 'nameValue' ] ] = $value;
+			}
+			else{
+				$newArr[ $final[ $key ] ] = $value;
+			}
+		}
+
+		echo json_encode($newArr);
+		return;
+
+
+
+
+
+		for($i=0;$i<count($ab);$i++){
+			var_dump( $ab[$i] ) ;
+			// $ab[$i]["name"] = $ab[$i]["name1"];
+			// unset($ab[$i]["name1"]);
+			var_dump( $ab[$i] ) ;			
+			break;
+		}
+		$this->disconnect();
 	}
 	function getAllDocs(){
 		echo "docs";
 	}
 
-// default functions
+	// default functions
 	function connect(){
-
+		try {
+			$this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
+    // set the PDO error mode to exception
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch(PDOException $e)
+		{
+			echo "Connection failed: " . $e->getMessage();
+		}
 	}
 	function disconnect(){
-		
+		$this->conn = null;
 	}
 }
 
